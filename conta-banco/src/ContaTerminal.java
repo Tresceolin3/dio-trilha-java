2ba3491da277fbe5262853130a4abb07f9fd7d21
Gabriel Ceolin <gabrieltresceolin@gmail.com>
2024-04-25 10:09:24 -0400

    adição da classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numero_conta;
+        String agencia, nome_cliente;
+        float saldo_conta;
+
+        System.out.println("Olá!, Seja Bem Vindo.");
+        System.out.println("Agradecemos a preferência!, insira o dados solicitados abaixo: \n");
+
+        System.out.print("Por favor, digite o seu nome completo: ");
+        nome_cliente = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        agencia = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        numero_conta = scanner.nextInt();
+        System.out.println();
+
+        System.out.print("Por favor, digite o valor a ser depositado: ");
+        saldo_conta = scanner.nextInt();
+        System.out.println();
+
+        System.out.println("Olá " + nome_cliente.toUpperCase()
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero_conta + " e seu saldo " + saldo_conta + " já esta disponível para saque");
+
     }
 }
